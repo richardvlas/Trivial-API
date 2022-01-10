b4ff1ef6038b2e81b11a0571aadb66d70c5468fa
Richard Vlas <vlasrichard@gmail.com>
2022-01-10 20:53:26 +0100

    added cors

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,18 +9,24 @@ from models import setup_db, Question, Category
 QUESTIONS_PER_PAGE = 10
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
   
 
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
-
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+    # Set up CORS. Allow '*' for origins.
+    CORS(app, resources={"/": {"origins": "*"}})
+  
+    # CORS Headers - Use the after_request decorator to set Access-Control-Allow
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
 
   '''
   @TODO: 
